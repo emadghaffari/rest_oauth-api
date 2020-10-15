4540c25bb1a983540a12517e14331a7268599a8e
Emad Ghaffari <emadghaffariii@gmail.com>
2020-10-15 14:13:09 +0000

    add service logic for accessTokens

--- src/domain/accesstoken/access_token.go
+++ src/domain/accesstoken/access_token.go
@@ -1,6 +1,11 @@
 package accesstoken
 
-import "time"
+import (
+	"strings"
+	"time"
+
+	"github.com/emadghaffari/bookstore_oauth-api/src/utils/errors"
+)
 
 const expireTime = 24
 
@@ -10,6 +15,23 @@ type AccessToken struct {
 	UserID      int64  `json:"user_id"`
 	ClientID    int64  `json:"client_id"`
 	Expires     int64  `json:"expires"`
+}
+
+func (at *AccessToken) Validate() *errors.ResError  {
+	at.AccessToken = strings.TrimSpace(at.AccessToken)
+	if at.AccessToken == ""{
+		return errors.HandlerBagRequest("invalid access token")
+	}
+	if at.ClientID <=0 {
+		return errors.HandlerBagRequest("invalid client id request")
+	}
+	if at.UserID <=0 {
+		return errors.HandlerBagRequest("invalid user id request")
+	}
+	if at.Expires <=0 {
+		return errors.HandlerBagRequest("invalid Expires request")
+	}
+	return nil
 }
 
 // GetNewAccessToken func
--- src/domain/accesstoken/service.go
+++ src/domain/accesstoken/service.go
@@ -1,8 +1,69 @@
 package accesstoken
 
-import "github.com/emadghaffari/bookstore_oauth-api/src/utils/errors"
+import (
+	"strings"
+
+	"github.com/emadghaffari/bookstore_oauth-api/src/utils/errors"
+)
+
+// Repository interface for create a new repo for this service
+type Repository interface {
+	GetByID(string) (*AccessToken, *errors.ResError)
+	Create(AccessToken) *errors.ResError
+	Update(AccessToken) *errors.ResError
+}
 
 // Service interface
 type Service interface {
-	GetByID(string) (*AccessToken, errors.ResError)
+	GetByID(string) (*AccessToken, *errors.ResError)
+	Create(AccessToken) *errors.ResError
+	Update(AccessToken) *errors.ResError
+}
+
+type service struct {
+	Repository Repository
+}
+
+// NewService func for create a new Service
+func NewService(repo Repository) Service {
+	return &service{
+		Repository: repo,
+	}
+}
+
+func (s service) GetByID(id string) (*AccessToken, *errors.ResError) {
+	id = strings.TrimSpace(id)
+	if len(id) == 0 {
+		return nil, errors.HandlerBagRequest("invalid access token")
+	}
+	access, err := s.Repository.GetByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return access, nil
+}
+
+func (s service) Create(ac AccessToken) *errors.ResError  {
+	if err := ac.Validate(); err != nil {
+		return err
+	}
+
+	err := s.Repository.Create(ac)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s service) Update(ac AccessToken) *errors.ResError  {
+	if err := ac.Validate(); err != nil {
+		return err
+	}
+	
+	err := s.Repository.Update(ac)
+	if err != nil {
+		return err
+	}
+	return nil
 }
